7e3ea1461d138f46fe7aadb3dabb53851ec3148e
Vishwa Muthulingam <151023895+muthulingamVishwa@users.noreply.github.com>
2024-08-26 22:20:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function otdate() {
+  let outputDate = document.querySelector('.outdate');
+  let inputDate = document.querySelector('.inputdate');
+  
+  if (inputDate && outputDate) {
+    console.log(inputDate.value);
+    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+    outputDate.value = formattedDate;
+  } else {
+    console.error("Input or output element not found");
+  }
+}

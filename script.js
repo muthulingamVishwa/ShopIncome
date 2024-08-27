45e64a9528ed8288ac48c066ce890c4a1b44de4b
Vishwa Muthulingam <151023895+muthulingamVishwa@users.noreply.github.com>
2024-08-27 08:08:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,5 @@ function capthcs(){
 document.querySelector('input[name="submit"]').classList.remove('hidden');
 
 }
-function onClick(e) {
-    e.preventDefault();
-    grecaptcha.enterprise.ready(async () => {
-      const token = await grecaptcha.enterprise.execute('6Le-kC8qAAAAAIat4Yua5YotP5UAxrpvUVOypgAC', {action: 'LOGIN'});
-    });
-  }
+
 
